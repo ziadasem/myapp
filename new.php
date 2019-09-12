cf3de6077ddb13a410ad998a88651ca23dcdc918
ziad asem <52223041+ziadasem@users.noreply.github.com>
2019-09-12 02:28:33 +0200

    Update new.php

--- new.php
+++ new.php
@@ -1,87 +1,35 @@
 <?php
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "almoazen";
-
-
-// Create connection
-$conn = new mysqli($servername, $username, $password ,$dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-
-$sql = "SELECT id  FROM almoazen";
-$result = $conn->query($sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"].  "<br>";
-        $user_name = '';
-    if (!empty($_SESSION['name'])) {
-     $user_name = $_SESSION['name'];
-     echo "echo"  + $user_name ;
-    }else{
-        echo "error";
-    }
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();*/
-
 $url = 'http://testwebapi.justdevelopmentjo.com/api/AlmozenRegistry/SaveMozenRegistry';
-$post_data = array(
-    "id" => "0",
-    "name" => "dummy",
-    "email" => "dummy",
-    "mobile" => "dummy",
-    "videoLink" => "dummy",
-    "country" => "dummy",
-    "bod" => "dummy",
+$data = array(
+    "id" => "16232323",
+    "message" => "3434" ,
 );
-$json = json_encode($post_data);
-$httpb= http_build_query($post_data);
-//echo ($json);
+$payload = json_encode($data);
+echo ($payload);
 
 $ch = curl_init($url);
 
-//curl_setopt($ch , CURLOPT_URL ,$url);
-
-curl_setopt($ch , CURLOPT_POST , 1 );
-
-curl_setopt($ch , CURLOPT_POSTFIELDS ,$json);
-
-curl_setopt($ch , CURLOPT_RETURNTRANSFER ,1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
 
 
-
-$output = curl_exec($ch);
-
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+  //  'Content-Length: ' . strlen($payload)
+    )
+);
 
 
-if ($output == FALSE){
+$result = curl_exec($ch);
+
+if ($result == FALSE){
     echo "curl error" .curl_error($ch);
 }
 
+print_r($result);
+
 curl_close($ch);
-print_r($output);
 
-/*$conn = new mysqli("SQL5045.site4now.net", "DB_A466D7_TestJustDev_admin", "Sa123@Sa1234" ,"AlmozenRegistry");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-
-$conn->close();*/
 ?>
